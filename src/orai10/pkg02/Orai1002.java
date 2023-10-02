7d50c51ba495c5f91bd7a8831825e5a2260cb5c2
sw4q3ar2 <biggercsgo@gmail.com>
2023-10-02 13:00:52 +0200

    orai munka 10.02

---
+++ src/orai10/pkg02/Orai1002.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package orai10.pkg02;
+
+/**
+ *
+ * @author palinkas.arnold
+ */
+public class Orai1002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
